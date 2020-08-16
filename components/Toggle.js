06479da37b7a653894c3a1c91aeb8e8796954f00
Sam <girlsam@hey.com>
2020-08-16 16:00:01 -0600

    Scaffold main page with dark mode

---
+++ components/Toggle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import useDarkMode from 'use-dark-mode';
+
+const Toggle = () => {
+  const darkMode = useDarkMode(false);
+
+  
+  return (
+    <button type="button" onClick={ darkMode.toggle }>
+      Toggle Theme
+    </button>
+  );
+}
+
+export default Toggle;
