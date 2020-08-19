1e14cafb2d04a296918a2690221623febe1747ac
Sam <girlsam@hey.com>
2020-08-19 09:40:48 -0600

    Debug dreaded flash

--- public/noflash.js
+++ public/noflash.js
@@ -10,6 +10,7 @@
     var classNameLight = 'light-mode';
   
     function setClassOnDocumentBody(darkMode) {
+      console.log(darkMode)
       document.body.classList.add(darkMode ? classNameDark : classNameLight);
       document.body.classList.remove(darkMode ? classNameLight : classNameDark);
     }
