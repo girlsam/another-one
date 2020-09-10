846a2fdf75bc70a07b2247a3920c294b4fd41b7c
Sam <girlsam@hey.com>
2020-09-10 11:35:58 -0600

    Refactor to use styled components lol

--- components/buttons/Toggle.js
+++ components/buttons/Toggle.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import useDarkMode from 'use-dark-mode';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 9rem;
+  height: 3rem;
+  background-color: ${ ({ darkMode, theme }) => 
+    darkMode.value ? theme.background.light : theme.background.dark };
+  border: 0;
+  border-radius: 40px;
+  color: ${({ darkMode, theme }) =>
+    darkMode.value ? theme.colors.light : theme.colors.dark
+  };
+  font-size: ${({ theme }) => theme.fontSizes.h6};
+  cursor: pointer;
+`;
 
 const Toggle = () => {
   const darkMode = useDarkMode(false);
 
   return (
-    <button
+    <Button
       type="button"
       aria-pressed={ darkMode.value }
       onClick={ () => darkMode.toggle() }
+      darkMode={ darkMode }
     >
-      Toggle {darkMode.value ? 'light' : 'dark'} mode
-    </button>
+      Toggle { darkMode.value ? 'light' : 'dark' } mode
+    </Button>
   );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.scss'
+import { ThemeProvider } from 'styled-components';
+
+import theme from '../styles/theme';
+
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ theme }>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
